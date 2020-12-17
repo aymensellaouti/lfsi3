e9ae37697030fc8085c63442b39f504c2c50205c
aymen2782 <aymen.sellaouti@gmail.com>
2020-12-17 16:47:00 +0100

    init add Personne

--- src/app/cv/add-personne/add-personne.component.ts
+++ src/app/cv/add-personne/add-personne.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-personne',
   templateUrl: './add-personne.component.html',
-  styleUrls: ['./add-personne.component.css']
+  styleUrls: ['./add-personne.component.css'],
 })
 export class AddPersonneComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  addPersonne(formumaire: NgForm) {}
 }
